db143f20fd954a4ca195e3451af99d0421e8fc4e
valentina <valentinagiannone@gmail.com>
2019-10-22 01:15:06 +0200

    finito
    aggiunto css

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -19,13 +19,16 @@ function gioco() {
   computer = ( "computer", Math.floor(Math.random()* 6 + 1));
   //console.log(computer);
 
+
   //eseguo comparazione numeri random
   if ( player > computer ){
-   //console.log("hai vinto"); 
+   //console.log("hai vinto");
    document.getElementById("btn").innerHTML = "Hai vinto!!!" + "il tuo risultato è " + player ;
+
   }else if (player < computer)
    //console.log("hai perso");
      document.getElementById("btn").innerHTML = "Hai perso!!!" + "il tuo risultato è " + computer ;
+
    else{
     //console.log("parità");
       document.getElementById("btn").innerHTML ="Il tuo punteggio è pari"
