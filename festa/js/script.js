64ed9c08a7cf82c9346b1f3a973988d8311b372f
valentina <valentinagiannone@gmail.com>
2019-10-21 14:49:56 +0200

    inserimento descrizione ex festa

---
+++ festa/js/script.js
@@ -0,0 +1,10 @@
+//ESERCIZIO 1 FESTA ( dopo aver portato fuori il cane) :) :
+// Chiedi all’utente la sua email
+//controlla che sia nella lista di chi può accedere
+// stampa un messaggio appropriato;
+
+//creo mia lista email
+//chiedo all'utente di inserire la sua email
+//verifico inserimento email utente
+//stampo messaggio se invitato alla FESTA
+//oppure stampo messaggio se non è invitato alla FESTA
