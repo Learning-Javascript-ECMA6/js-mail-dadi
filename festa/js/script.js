28e67e26b43204f0a2d5fc67d45a531a17a16d5b
valentina <valentinagiannone@gmail.com>
2019-10-21 16:47:12 +0200

    verifico inserimento dati utente

--- festa/js/script.js
+++ festa/js/script.js
@@ -15,3 +15,18 @@ console.log(list);
 
 //chiedo all'utente di inserire la sua email utilizzando prompt e assegnando variabile input
 var mail = prompt("ciao, inserisci il tuo indirizzo mail per partecipare alla festa");
+
+//verifico inserimento email utente
+for (var i = 0; i <= list.length; i++) {
+ if (mail === list[i]){
+  trovato = true ;
+  //se è in lista partecipa
+  console.log("benvenuto alla festa" );
+  var invito = true;
+ }
+}
+
+//se l email non è in lista non partecipa
+if (invito !== true){
+ console.log("non sei nella lista");
+}
