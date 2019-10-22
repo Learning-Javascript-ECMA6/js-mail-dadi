a48b656fd923c5234c879b4ecf8e65d9400fb03e
valentina <valentinagiannone@gmail.com>
2019-10-22 13:08:23 +0200

    FINITO
    aggiunto css e finito

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,13 +2,16 @@
 
 //assegno variabile giocatori
 // eseguo calcolo random
-//stampo il risultato
+//eseguo comparazione per far comparire le immagini durante il gioco e stampo
+//eseguo altra comparazione per risultato scritto e stampo
+
+
 
 // assegno variabile giocatori
 var player, computer;
 
 //inserisco funzione per associare il risultato al button
-function gioco() {
+
 //console.log(gioco);
 
   //scrivo formula per far comparire numero random giocatore 1
@@ -19,18 +22,47 @@ function gioco() {
   computer = ( "computer", Math.floor(Math.random()* 6 + 1));
   //console.log(computer);
 
+ // inserisco immagini da far comparire durante il gioco - vittoria player
+  if (player == 1) {
+    document.getElementById("result1").innerHTML = "<img src='../dadi/img/uno.jpg'>";
+  } else if (player == 2) {
+    document.getElementById("result1").innerHTML = "<img src='../dadi/img/due.png'>";
+  } else if (player == 3) {
+    document.getElementById("result1").innerHTML = "<img src='../dadi/img/tre.jpg'>";
+  } else if (player == 4) {
+    document.getElementById("result1").innerHTML = "<img src='../dadi/img/quattro.jpg'>";
+  } else if (player == 5) {
+    document.getElementById("result1").innerHTML = "<img src='../dadi/img/cinque.png'>";
+  } else if (player == 6) {
+    document.getElementById("result1").innerHTML = "<img src='../dadi/img/sei.jpg'>";
+  }
+
+  // inserisco immagini da far comparire durante il gioco - vittoria computer
+  if (computer == 1) {
+    document.getElementById("result2").innerHTML = "<img src='../dadi/img/uno.jpg'>";
+  } else if (computer == 2) {
+    document.getElementById("result2").innerHTML = "<img src='../dadi/img/due.png'>";
+  } else if (computer == 3) {
+    document.getElementById("result2").innerHTML = "<img src='../dadi/img/tre.jpg'>";
+  } else if (computer == 4) {
+    document.getElementById("result2").innerHTML = "<img src='../dadi/img/quattro.jpg'>";
+  } else if (computer == 5) {
+    document.getElementById("result2").innerHTML = "<img src='../dadi/img/cinque.png'>";
+  } else if (computer == 6) {
+    document.getElementById("result2").innerHTML = "<img src='../dadi/img/sei.jpg'>";
+  }
+
 
   //eseguo comparazione numeri random
   if ( player > computer ){
    //console.log("hai vinto");
-   document.getElementById("btn").innerHTML = "Hai vinto!!!" + "il tuo risultato è " + player ;
+   document.getElementById("winfinal").innerHTML = "Hai vinto!!! " + " il tuo risultato è " + player ;
 
   }else if (player < computer)
    //console.log("hai perso");
-     document.getElementById("btn").innerHTML = "Hai perso!!!" + "il tuo risultato è " + computer ;
+     document.getElementById("winfinal").innerHTML = "Hai perso!!! " + " il tuo risultato è " + computer ;
 
    else{
     //console.log("parità");
-      document.getElementById("btn").innerHTML ="Il tuo punteggio è pari"
+      document.getElementById("winfinal").innerHTML ="Il tuo punteggio è di parità"
    }
-}
